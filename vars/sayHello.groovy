76b937f7ffb77443c57383cedabc1405d82b0cf0
Julien Viet <julien@julienviet.com>
2018-05-06 17:55:30 +0200

    First version that trigger the build workflow using a project matrix

--- vars/sayHello.groovy
+++
@@ -1,3 +0,0 @@
-def call(String name = "human") {
-    echo "Hello, ${name}"
-}
---
+++ vars/triggerWorkflow.groovy
@@ -0,0 +1,108 @@
+import groovy.transform.Field
+
+@Field
+Map<String, List<String>> dependencyGraph = [
+    "3.5": [
+        "vertx-codegen"      : [],
+        "vertx-core"         : [ "vertx-codegen" ],
+        "vertx-unit"         : ["vertx-core"],
+        "vertx-sql-common"   : ["vertx-core"],
+        "vertx-jdbc-client"  : ["vertx-sql-common"],
+        "vertx-auth"         : ["vertx-core", "vertx-jdbc-client", "vertx-mongo-client"],
+        "vertx-mongo-client" : ["vertx-core"],
+        "vertx-bridge-common": ["vertx-core"],
+        "vertx-web"          : ["vertx-core", "vertx-auth", "vertx-bridge-common", "vertx-unit"],
+    ],
+    "master": [
+        "vertx-codegen"      : [],
+        "vertx-core"         : [ "vertx-codegen" ],
+        "vertx-unit"         : ["vertx-core"],
+        "vertx-sql-common"   : ["vertx-core"],
+        "vertx-jdbc-client"  : ["vertx-sql-common"],
+        "vertx-auth"         : ["vertx-core", "vertx-jdbc-client", "vertx-mongo-client"],
+        "vertx-mongo-client" : ["vertx-core"],
+        "vertx-bridge-common": ["vertx-core"],
+        "vertx-web"          : ["vertx-core", "vertx-auth", "vertx-bridge-common", "vertx-unit"],
+    ]
+]
+
+// Perform a topological sort of the set according to the dependency graph
+def topologicalVisit(String version, String id, List<String> sorted) {
+    def dependencies = [] as Set
+    for (entry in dependencyGraph[version]) {
+        if (entry.value.contains(id)) {
+            dependencies.add(entry.key)
+        }
+    }
+    for (dependency in dependencies) {
+        if (!sorted.contains(dependency)) {
+            topologicalVisit(version, dependency, sorted)
+        }
+    }
+    sorted.add(id)
+}
+
+def topologicalSort(String version, String id) {
+    def sorted = [] as List
+    topologicalVisit(version, id, sorted)
+    def result = [] as List
+    // Sort in correct order and omit first element (that is self)
+    for (int i = sorted.size() - 2;i >= 0;i--) {
+        result.add(sorted[i])
+    }
+    return result
+}
+
+// For testing purpose in IDE
+def test() {
+    def closure = topologicalSort("vertx-codegen")
+    println closure
+}
+
+test()
+
+def call() {
+
+    // Find if we have an existing job list in params
+    def jobList = params.jobList
+    if (jobList != null) {
+        echo "Triggered by job list [$jobList]"
+        if (jobList.length() > 0) {
+            int idx = jobList.indexOf(',')
+            def nextJob = idx == - 1 ? jobList : jobList.substring(0, idx)
+            def nextJobList = jobList.substring(idx + 1)
+            build job: nextJob, wait: false, parameters: [[$class: 'StringParameterValue', name: 'jobList', value: nextJobList]]
+        }
+    } else {
+        int idx
+        def job = "${env.JOB_NAME}"
+        idx = job.lastIndexOf('/')
+        if (idx == -1) {
+            echo "Invalid job name ${env.JOB_NAME}"
+            return
+        }
+        def version = job.substring(idx + 1)
+        job = job.substring(0, idx)
+        idx = job.lastIndexOf('/')
+        if (idx == -1) {
+            echo "Invalid job name ${env.JOB_NAME}"
+            return
+        }
+        def id = job.substring(idx + 1)
+        def prefix = job.substring(0, idx)
+
+        def dependencies = topologicalSort(version, id)
+        echo "${id} -> ${dependencies}"
+        if (dependencies.size() > 0) {
+            def nextJob = "$prefix/${dependencies[0]}/${version}"
+            def nextJobList = ""
+            for (int i = 1;i < dependencies.size();i++) {
+                if (i > 1) {
+                    nextJobList += ","
+                }
+                nextJobList += "$prefix/${dependencies[i]}/${version}"
+            }
+            build job: nextJob, wait: false, parameters: [[$class: 'StringParameterValue', name: 'jobList', value: nextJobList]]
+        }
+    }
+}
