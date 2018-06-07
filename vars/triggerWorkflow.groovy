ad84728e0e627d2230ba71d5e94bafc2d28d680b
Julien Viet <julien@julienviet.com>
2018-06-07 13:09:05 +0200

    More

--- vars/triggerWorkflow.groovy
+++ vars/triggerWorkflow.groovy
@@ -3,84 +3,86 @@ import groovy.transform.Field
 @Field
 Map<String, List<String>> dependencyGraph = [
     "3.5": [
-        "vertx-codegen"               : [],
-        "vertx-core"                  : ["vertx-codegen"],
+        "vertx-codegen"                 : [],
+        "vertx-core"                    : ["vertx-codegen"],
 
         // Programming
-        "vertx-rx"                    : ["vertx-core"],
-        "vertx-reactive-streams"      : ["vertx-core"],
-        "vertx-sync"                  : ["vertx-core"],
+        "vertx-rx"                      : ["vertx-core"],
+        "vertx-reactive-streams"        : ["vertx-core"],
+        "vertx-sync"                    : ["vertx-core"],
 
         // Langs
-        "vertx-lang-groovy"           : ["vertx-core"],
-        "vertx-lang-js"               : ["vertx-core"],
-        "vertx-lang-ruby"             : ["vertx-core"],
-        "vertx-lang-kotlin"           : ["vertx-core", "vertx-rx"],
+        "vertx-lang-groovy"             : ["vertx-core"],
+        "vertx-lang-js"                 : ["vertx-core"],
+        "vertx-lang-ruby"               : ["vertx-core"],
+        "vertx-lang-kotlin"             : ["vertx-core", "vertx-rx"],
 
         // Service factory
-        "vertx-service-factory"       : ["vertx-core"],
-        "vertx-maven-service-factory" : ["vertx-service-factory"],
-        "vertx-http-service-factory"  : ["vertx-service-factory"],
+        "vertx-service-factory"         : ["vertx-core"],
+        "vertx-maven-service-factory"   : ["vertx-service-factory"],
+        "vertx-http-service-factory"    : ["vertx-service-factory"],
 
         // Services
-        "vertx-service-proxy"         : ["vertx-core", "vertx-auth"],
-        "vertx-grpc"                  : ["vertx-core"],
+        "vertx-service-proxy"           : ["vertx-core", "vertx-auth"],
+        "vertx-grpc"                    : ["vertx-core"],
+        "vertx-client-services"         : ["vertx-mongo-client", "vertx-consul-client", "vertx-mail-client"],
 
         // Testing
-        "vertx-unit"                  : ["vertx-rx", "vertx-lang-groovy", "vertx-lang-js", "vertx-lang-ruby", "vertx-lang-kotlin"],
-        "vertx-junit5"                : ["vertx-core"],
+        "vertx-unit"                    : ["vertx-rx", "vertx-lang-groovy", "vertx-lang-js", "vertx-lang-ruby", "vertx-lang-kotlin"],
+        "vertx-junit5"                  : ["vertx-core"],
 
         // Legacy
-        "vertx-jca"                   : ["vertx-core"],
+        "vertx-jca"                     : ["vertx-core"],
 
         // Clustering
-        "vertx-hazelcast"             : ["vertx-core", "vertx-web"],
-        "vertx-infinispan"            : ["vertx-core", "vertx-web"],
-        "vertx-zookeeper"             : ["vertx-core", "vertx-web"],
-        "vertx-ignite"                : ["vertx-core", "vertx-web"],
+        "vertx-hazelcast"               : ["vertx-core", "vertx-web"],
+        "vertx-infinispan"              : ["vertx-core", "vertx-web"],
+        "vertx-zookeeper"               : ["vertx-core", "vertx-web"],
+        "vertx-ignite"                  : ["vertx-core", "vertx-web"],
 
         // Messaging
-        "vertx-proton"                : ["vertx-unit"],
-        "vertx-amqp-bridge"           : ["vertx-proton"],
-        "vertx-stomp"                 : ["vertx-unit"],
-        "vertx-camel-bridge"          : ["vertx-unit", "vertx-stomp"],
-        "vertx-kafka-client"          : ["vertx-unit"],
-        "vertx-mqtt"                  : ["vertx-unit"],
-        "vertx-rabbitmq-client"       : ["vertx-unit"],
+        "vertx-proton"                  : ["vertx-unit"],
+        "vertx-amqp-bridge"             : ["vertx-proton"],
+        "vertx-stomp"                   : ["vertx-unit"],
+        "vertx-camel-bridge"            : ["vertx-unit", "vertx-stomp"],
+        "vertx-kafka-client"            : ["vertx-unit"],
+        "vertx-mqtt"                    : ["vertx-unit"],
+        "vertx-rabbitmq-client"         : ["vertx-unit"],
 
         // Bridge
-        "vertx-bridge-common"         : ["vertx-rx", "vertx-lang-groovy", "vertx-lang-js", "vertx-lang-ruby", "vertx-lang-kotlin"],
-        "vertx-tcp-eventbus-bridge"   : ["vertx-core", "vertx-bridge-common"],
+        "vertx-bridge-common"           : ["vertx-rx", "vertx-lang-groovy", "vertx-lang-js", "vertx-lang-ruby", "vertx-lang-kotlin"],
+        "vertx-tcp-eventbus-bridge"     : ["vertx-core", "vertx-bridge-common"],
 
         // Data
-        "vertx-embedded-mongo-db"     : ["vertx-service-factory"],
-        "vertx-sql-common"            : ["vertx-rx", "vertx-lang-groovy", "vertx-lang-js", "vertx-lang-ruby", "vertx-lang-kotlin"],
-        "vertx-mongo-client"          : ["vertx-rx", "vertx-lang-groovy", "vertx-lang-js", "vertx-lang-ruby", "vertx-lang-kotlin"],
-        "vertx-redis-client"          : ["vertx-rx", "vertx-lang-groovy", "vertx-lang-js", "vertx-lang-ruby", "vertx-lang-kotlin"],
-        "vertx-jdbc-client"           : ["vertx-sql-common"],
-        "vertx-consul-client"         : ["vertx-unit", "vertx-web"],
+        "vertx-embedded-mongo-db"       : ["vertx-service-factory"],
+        "vertx-sql-common"              : ["vertx-rx", "vertx-lang-groovy", "vertx-lang-js", "vertx-lang-ruby", "vertx-lang-kotlin"],
+        "vertx-mongo-client"            : ["vertx-rx", "vertx-lang-groovy", "vertx-lang-js", "vertx-lang-ruby", "vertx-lang-kotlin"],
+        "vertx-redis-client"            : ["vertx-rx", "vertx-lang-groovy", "vertx-lang-js", "vertx-lang-ruby", "vertx-lang-kotlin"],
+        "vertx-jdbc-client"             : ["vertx-sql-common"],
+        "vertx-mysql-postgresql-client" : ["vertx-sql-common"],
+        "vertx-consul-client"           : ["vertx-unit", "vertx-web"],
 
         // Mail
-        "vertx-mail-client"           : ["vertx-unit"],
+        "vertx-mail-client"             : ["vertx-unit"],
 
         // Security
-        "vertx-auth"                  : ["vertx-jdbc-client", "vertx-mongo-client"],
+        "vertx-auth"                    : ["vertx-jdbc-client", "vertx-mongo-client"],
 
         // Microservices
-        "vertx-circuit-breaker"       : ["vertx-rx", "vertx-lang-groovy", "vertx-lang-js", "vertx-lang-ruby", "vertx-lang-kotlin"],
-        "vertx-service-discovery"     : ["vertx-hazelcast", "vertx-web", "vertx-jdbc-client", "vertx-redis-client", "vertx-mongo-client", "vertx-consul-client", "vertx-service-proxy"],
-        "vertx-health-check"          : ["vertx-web", "vertx-auth", "vertx-service-discovery", "vertx-unit"],
-        "vertx-config"                : ["vertx-web", "vertx-redis-client", "vertx-consul-client"],
+        "vertx-circuit-breaker"         : ["vertx-rx", "vertx-lang-groovy", "vertx-lang-js", "vertx-lang-ruby", "vertx-lang-kotlin"],
+        "vertx-service-discovery"       : ["vertx-hazelcast", "vertx-web", "vertx-jdbc-client", "vertx-redis-client", "vertx-mongo-client", "vertx-consul-client", "vertx-service-proxy"],
+        "vertx-health-check"            : ["vertx-web", "vertx-auth", "vertx-service-discovery", "vertx-unit"],
+        "vertx-config"                  : ["vertx-web", "vertx-redis-client", "vertx-consul-client"],
 
         // Web
-        "vertx-web"                   : ["vertx-auth", "vertx-bridge-common", "vertx-unit"],
+        "vertx-web"                     : ["vertx-auth", "vertx-bridge-common", "vertx-unit"],
 
         // Shell
-        "vertx-shell"                 : ["vertx-unit", "vertx-auth", "vertx-web"],
+        "vertx-shell"                   : ["vertx-unit", "vertx-auth", "vertx-web"],
 
         // Metrics
-        "vertx-dropwizard-metrics"    : ["vertx-shell", "vertx-service-factory"],
-        "vertx-micrometer-metrics"    : ["vertx-unit", "vertx-web"]
+        "vertx-dropwizard-metrics"      : ["vertx-shell", "vertx-service-factory"],
+        "vertx-micrometer-metrics"      : ["vertx-unit", "vertx-web"]
     ],
     "master": [
         "vertx-codegen"      : [],
